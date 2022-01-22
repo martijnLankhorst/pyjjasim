28faec9af5c5e6e2a3e2c2dd640913a32b6e4997
Martijn Lankhorst <m.lankhorst89@gmail.com>
2022-01-22 03:47:04 +0100

    updated doc

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../pyjjasim/'))
+sys.path.insert(0, os.path.abspath('./pyjjasim/'))
 
 print(sys.path)
 # -- Project information -----------------------------------------------------
